479908d40fec75a751073d77fd0c64349f648da8
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-09-23 01:33:40 +0530

    446. Login Page

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,25 +1,15 @@
 const FormInput = ({ label, name, type, defaultValue }) => {
   return (
-    // <div className="form-control w-full max-w-xs">
     <div className="form-control ">
       <label className="label">
-        {/* <span className="label-text">What is your name?</span> */}
         <span className="label-text">{label}</span>
-        {/* <span className="label-text-alt">Top Right label</span> */}
       </label>
       <input
-        // type="text"
         type={type}
         name={name}
         defaultValue={defaultValue}
-        // placeholder="Type here"
-        // className="input input-bordered w-full max-w-xs"
         className="input input-bordered "
       />
-      {/* <label className="label">
-        <span className="label-text-alt">Bottom Left label</span>
-        <span className="label-text-alt">Bottom Right label</span>
-      </label> */}
     </div>
   );
 };
