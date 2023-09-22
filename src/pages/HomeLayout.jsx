eba1c3262c651d75c8e5bf213143c1c813f7438a
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-09-23 04:31:18 +0530

    449. Custom Class

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
-  // return <h1 className="text-4xl">HomeLayout</h1>;
-
   return (
     <>
       <nav>
         <span className="text-4xl text-primary">comfy</span>
       </nav>
-      <Outlet />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };
